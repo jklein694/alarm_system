5614616ebdf68794ba9d957630f46067c43544ea
Joe Klein <jklein694@gmail.com>
2017-12-28 19:49:42 -0600

    adding directional movement

--- real_time_object_detection.py
+++ real_time_object_detection.py
@@ -33,10 +33,16 @@ COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 width_loc = {}
 height_loc = {}
+area_dict = {}
+area_per_list = []
 
 for item in CLASSES:
     width_loc[item] = []
     height_loc[item] = []
+    area_dict[item] = []
+
+area_dict['person'].append(5000)
+person_counter = 0
 
 # load our serialized model from disk
 print("[INFO] loading model...")
@@ -69,6 +75,7 @@ while True:
     detections = net.forward()
 
     direction = []
+    area_direction = []
 
     # loop over the detections
     for i in np.arange(0, detections.shape[2]):
@@ -89,54 +96,96 @@ while True:
 
             (startX, startY, endX, endY) = box.astype("int")
 
-            width = int((endX + startX) / 2)
-            height = int((endY + startY) / 2)
+            if CLASSES[idx] == 'person' or 'dog':
+                width = int((endX + startX) / 2)
+                height = int((endY + startY) / 2)
 
-            center = (width, height)
+                area = int((endX - startX) * (endY - startY))
 
-            # draw the prediction on the frame
-            label = "{}: {:.2f}%".format(CLASSES[idx],
-                                         confidence * 100)
-            cv2.rectangle(frame, (startX, startY), (endX, endY),
-                          COLORS[idx], 2)
-            cv2.circle(frame, (width, height), 2, COLORS[idx], 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(frame, label, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                center = (width, height)
 
-            if CLASSES[idx] == 'person':
-                width_loc[CLASSES[idx]].append(width)
-                height_loc[CLASSES[idx]].append(height)
+                # draw the prediction on the frame
+                label = "{}: {:.2f}%".format(CLASSES[idx],
+                                             confidence * 100)
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                              COLORS[idx], 2)
+                cv2.circle(frame, (width, height), 2, COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
-            for n in range(len(width_loc['person'])):
-                try:
-                    slope = width_loc['person'][n + 10] - width_loc['person'][n]
-                    direction.append(slope)
-                except:
-                    None
+                if CLASSES[idx] == 'person':
+                    person_counter += 1
 
-            chunks = cc.create_chunks(direction, 10)
+                    width_loc[CLASSES[idx]].append(width)
+                    height_loc[CLASSES[idx]].append(height)
 
-            average_slope = []
-            movement = 0
-            for chunk in chunks:
-                average_slope.append(np.mean(chunk))
-                movement = np.mean(chunk)
+                    area = int((endX - startX) * (endY - startY))
+                    area_dict[CLASSES[idx]].append(area)
 
-            speed_label = "{}: {:.2f}".format('Speed: ', movement)
+                    area_per_list.append(area_dict['person'][person_counter - 1] / area_dict['person'][person_counter])
 
-            if movement <= -20:
-                direction_label = 'Moving Right'
-            elif movement >= 20:
-                direction_label = 'Moving Left'
-            else:
-                direction_label = ''
 
-            cv2.putText(frame, direction_label, (startX + 150, y - 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-            cv2.putText(frame, speed_label, (startX + 150, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-            print(direction_label)
+                for n in range(len(width_loc['person'])):
+                    try:
+                        slope = width_loc['person'][n + 10] - width_loc['person'][n]
+                        direction.append(slope)
+                        slope_area = area_per_list[n + 50] - area_per_list[n]
+                        area_direction.append(slope_area)
+                    except:
+                        None
+
+                chunks = cc.create_chunks(direction, 10)
+                chunks_area = cc.create_chunks(area_direction, 30)
+
+                average_slope = []
+                average_area_slope = []
+                movement = 0
+                for chunk in chunks:
+                    average_slope.append(np.mean(chunk))
+                    movement = np.mean(chunk)
+
+                area_movement = 0
+                for chunk_area in chunks_area:
+                    average_area_slope.append(np.mean(chunk_area))
+                    area_movement = np.mean(chunk_area)
+
+                lng_move_list = []
+                lng_move = 0
+                for index in range(len(average_area_slope)):
+                    try:
+                        lng_move = average_area_slope[index] - average_area_slope[index - 1]
+                        lng_move_list.append(lng_move)
+                    except:
+                        None
+
+                speed = np.mean([movement, area_movement])
+                speed_label = "{}: {:.2f}".format('Speed: ', speed)
+
+                if movement <= -20:
+                    direction_label = 'Moving Right'
+                elif movement >= 20:
+                    direction_label = 'Moving Left'
+                else:
+                    direction_label = ''
+
+                if lng_move > 0:
+                    area_direction_label = 'Moving Away'
+                elif lng_move < 0:
+                    area_direction_label = 'Moving Towards'
+                else:
+                    area_direction_label = ''
+
+                cv2.putText(frame, direction_label, (startX + 150, y + 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+                cv2.putText(frame, area_direction_label, (startX + 150, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+                cv2.putText(frame, speed_label, (startX, y + 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+
 
     # show the output frame
     cv2.imshow("Frame", frame)
@@ -167,6 +216,10 @@ print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 direction = []
 
 plt.plot(average_slope)
+plt.show()
+plt.plot(average_area_slope, label='area change')
+plt.plot(lng_move_list, label='area change slope')
+plt.legend()
 plt.show()
 
 # do a bit of cleanup
