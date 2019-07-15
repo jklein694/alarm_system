8dd87b8a05868befbfd9441ff63bdb35752bc0d0
Joe Klein <jklein694@gmail.com>
2019-07-14 22:28:09 -0400

    update

--- run_motor.py
+++ run_motor.py
@@ -7,23 +7,24 @@ Motor1A = 16
 Motor1B = 18
 Motor1E = 22
  
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1E,GPIO.OUT)
- 
-print("Going forwards")
-GPIO.output(Motor1A,GPIO.HIGH)
-GPIO.output(Motor1B,GPIO.LOW)
-GPIO.output(Motor1E,GPIO.HIGH)
- 
-sleep(5)
- 
-print("Going backwards")
-GPIO.output(Motor1A,GPIO.LOW)
-GPIO.output(Motor1B,GPIO.HIGH)
-GPIO.output(Motor1E,GPIO.HIGH)
- 
-sleep(5)
+for _ in range(30):
+    GPIO.setup(Motor1A,GPIO.OUT)
+    GPIO.setup(Motor1B,GPIO.OUT)
+    GPIO.setup(Motor1E,GPIO.OUT)
+    
+    print("Going forwards")
+    GPIO.output(Motor1A,GPIO.HIGH)
+    GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1E,GPIO.HIGH)
+    
+    sleep(4)
+    
+    print("Going backwards")
+    GPIO.output(Motor1A,GPIO.LOW)
+    GPIO.output(Motor1B,GPIO.HIGH)
+    GPIO.output(Motor1E,GPIO.HIGH)
+    
+    sleep(4)
  
 print("Now stop")
 GPIO.output(Motor1E,GPIO.LOW)
