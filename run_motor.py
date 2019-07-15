8c18735c954dc01eeee127d9e800f1b4cabce7bf
Joe Klein <jklein694@gmail.com>
2019-07-14 22:53:13 -0400

    update

--- run_motor.py
+++ run_motor.py
@@ -1,32 +1,55 @@
-import RPi.GPIO as GPIO
-from time import sleep
+# import RPi.GPIO as GPIO
+# from time import sleep
  
-GPIO.setmode(GPIO.BOARD)
+# GPIO.setmode(GPIO.BOARD)
 
-Motor1A = 16
-Motor1B = 18
-Motor1E = 22
+# Motor1A = 16
+# Motor1B = 18
+# Motor1E = 22
  
-for _ in range(2):
-    GPIO.setup(Motor1A,GPIO.OUT)
-    GPIO.setup(Motor1B,GPIO.OUT)
-    GPIO.setup(Motor1E,GPIO.OUT)
+# for _ in range(2):
+#     GPIO.setup(Motor1A,GPIO.OUT)
+#     GPIO.setup(Motor1B,GPIO.OUT)
+#     GPIO.setup(Motor1E,GPIO.OUT)
     
-    print("Going forwards")
-    GPIO.output(Motor1A,GPIO.HIGH)
-    GPIO.output(Motor1B,GPIO.LOW)
-    GPIO.output(Motor1E,GPIO.HIGH)
+#     print("Going forwards")
+#     GPIO.output(Motor1A,GPIO.HIGH)
+#     GPIO.output(Motor1B,GPIO.LOW)
+#     GPIO.output(Motor1E,GPIO.HIGH)
     
-    sleep(4)
+#     sleep(4)
     
-    print("Going backwards")
-    GPIO.output(Motor1A,GPIO.LOW)
-    GPIO.output(Motor1B,GPIO.HIGH)
-    GPIO.output(Motor1E,GPIO.HIGH)
+#     print("Going backwards")
+#     GPIO.output(Motor1A,GPIO.LOW)
+#     GPIO.output(Motor1B,GPIO.HIGH)
+#     GPIO.output(Motor1E,GPIO.HIGH)
     
-    sleep(4)
+#     sleep(4)
  
-print("Now stop")
-GPIO.output(Motor1E,GPIO.LOW)
+# print("Now stop")
+# GPIO.output(Motor1E,GPIO.LOW)
  
-GPIO.cleanup()
+# GPIO.cleanup()
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+
+GPIO.setup(6, GPIO.OUT) # output rf
+
+# Initial state for LEDs:
+print("Testing RF out, Press CTRL+C to exit")
+
+try:
+     print("set GIOP high")
+     GPIO.output(6, GPIO.HIGH)
+     time.sleep(5)               
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+   print("Keyboard interrupt")
+
+except:
+   print("some error") 
+
+finally:
+   print("clean up") 
+   GPIO.cleanup() # cleanup all GPIO 
